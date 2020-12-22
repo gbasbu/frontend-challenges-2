de73c7af0384b6e387fb696d140275714753aca2
gbasbu <gorkembasbu@gmail.com>
2020-12-22 15:44:24 +0300

    completed

---
+++ faq-accordion-card-main/js/script.js
@@ -0,0 +1,7 @@
+const acordion = document.getElementsByClassName('contentBox')
+
+for(i=0; i<acordion.length; i++) {
+    acordion[i].addEventListener('click', function() {
+        this.classList.toggle('active')
+    })
+}
